1af7d262a8d1eff977f97b7f55b27c6a56627554
Kyle Shay <kyle.shay@ymail.com>
2015-04-12 03:23:44 -0400

    update icon based on scroll pos

--- scrollnav.js
+++ scrollnav.js
@@ -1,26 +1,46 @@
-function scrollnav(options) {
+var scrollnav = (function () {
+	'use strict';
 
-  var nav = options.nav;
-  var nav = document.getElementById('nav');
+  var content, title, row, nav, hover, rows, offset;
 
-  var content = options.content || '.nav-content';
-  var title = options.title || '.title';
-  var row = options.row || '.row';
+  var init = function(options) {
+    if(!options) options = {}
+    nav = options.nav || document.getElementById('nav');
+    content = options.content || '.nav-content';
+    title = options.title || '.title';
+    row = options.row || '.row';
+    offset = options.offset || 0;
 
-  var hover = document.createElement('div');
-  hover.id = 'hover';
-  hover.className = 'hover';
-  document.body.appendChild(hover);
+    hover = document.createElement('div');
+    hover.id = 'hover';
+    hover.className = 'hover';
+    document.body.appendChild(hover);
 
-  var rows = document.querySelectorAll(row);
-  var padding = Math.floor(window.screen.height / (rows.length + 1) / 8);
-  for(var i = 0; i < rows.length; i++) {
-    var li = document.createElement('li');
-    li.innerHTML = '&#9900;';
-    li.style.padding = padding + 'px'
-    li.dataset.id = rows[i].querySelector(content).className.replace('nav-content ', '');
-    li.dataset.title = rows[i].querySelector(title).innerText;
-    nav.appendChild(li);
+    rows = document.querySelectorAll(row);
+    var padding = Math.floor(window.screen.height / (rows.length + 1) / 8);
+    for(var i = 0; i < rows.length; i++) {
+      var li = document.createElement('li');
+      li.innerHTML = '&#9900;';
+      li.style.padding = padding + 'px'
+      li.dataset.id = rows[i].querySelector(content).className.replace('nav-content ', '');
+      li.dataset.title = rows[i].querySelector(title).innerText;
+
+      if(i === 0) {
+        li.innerHTML = '&#8962;';
+        li.className = 'selected';
+      }
+      nav.appendChild(li);
+    }
+
+    nav.addEventListener('touchstart', start, false);
+    nav.addEventListener('mousedown', start, false);
+    nav.addEventListener('touchend', end, false);
+    nav.addEventListener('mouseup', end, false);
+    nav.addEventListener('touchmove', move, false);
+    nav.addEventListener('mousemove', move, false);
+    document.addEventListener('scroll', function(e) {
+      debounce(scroll(e), 250);
+    }, false);
   }
 
   var current = 0;
@@ -58,6 +78,59 @@ function scrollnav(options) {
     }
   }
 
+  function scroll(e) {
+    // for(var r = 0; r < rows.length; r++) {
+    //   // console.log('adsf', rows[r].querySelector('.title').classList.remove)
+    //   rows[r].querySelector('.title').classList.remove('sticky');
+    // }
+    console.log(nav.children.length)
+    // for(var c = 0; r < nav.children.length; c++) {
+    //   // console.log('adsf', rows[r].querySelector('.title').classList.remove)
+    //   nav.children[c].className = '';
+    // }
+    //
+    for(var s = 0; s < nav.childNodes.length; s++) {
+      nav.childNodes[s].className = '';
+    }
+    var sTop = document.body.scrollTop;
+    for(var r in rows) {
+      // console.log(r, rows[r], rows[r].scrollHeight, document.body.scrollTop)
+      if(rows[r].getBoundingClientRect().top + window.pageYOffset - offset > sTop) {
+        // el.getBoundingClientRect().top + window.pageYOffset - el.ownerDocument.documentElement.clientTop
+        // console.log('hey', r, rows[r]/)
+        break;
+      }
+    }
+
+    document.getElementById('thetitle').innerText = sTop < offset ? '' : rows[r-1].querySelector('.title').innerText.trim();
+    nav.children[r-1].className = 'selected';
+    // console.log(rows[r-1].innerText.trim())
+    // if(document.body.scrollTop >
+    // var window_top = $(window).scrollTop() + 12; // the "12" should equal the margin-top value for nav.stick
+    // var div_top = $('#nav-anchor').offset().top;
+    //     if (window_top > div_top) {
+    //         $('nav').addClass('stick');
+    //     } else {
+    //         $('nav').removeClass('stick');
+    //     }
+  }
+
+  var debounce = function(func, wait, immediate) {
+    var timeout, result;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) result = func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) result = func.apply(context, args);
+      return result;
+    };
+  };
+
   function valid(e) {
     return e != null && (e.id === 'nav' || e.parentNode.id === 'nav');
   }
@@ -74,10 +147,7 @@ function scrollnav(options) {
     return e;
   }
 
-  nav.addEventListener('touchstart', start, false);
-  nav.addEventListener('mousedown', start, false);
-  nav.addEventListener('touchend', end, false);
-  nav.addEventListener('mouseup', end, false);
-  nav.addEventListener('touchmove', move, false);
-  nav.addEventListener('mousemove', move, false);
-}
+  return {
+    init: init
+  }
+})();
